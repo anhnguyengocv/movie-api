22b3a425de2cee18be7a3457ba88b299619d95b3
anhnguyengocv <144266029+anhnguyengocv@users.noreply.github.com>
2024-03-24 02:04:08 -0500

    Update models.js

--- models.js
+++ models.js
@@ -1,16 +1,13 @@
 const mongoose = require("mongoose");
 
 let movieSchema = mongoose.Schema({
-    _id: {type: mongoose.Schema.Types.ObjectId, ref: 'movies'},
     title: {type: String, required: true},
     description: {type: String, required: true},
     genre: {
-      _id: {type: mongoose.Schema.Types.ObjectId, ref: 'genres'},
       name: String,
       description: String
     },
     director: {
-      _id: {type: mongoose.Schema.Types.ObjectId, ref: 'directors'},
       name: String,
       bio: String,
       birth: String,
@@ -20,34 +17,15 @@ let movieSchema = mongoose.Schema({
   });
   
 let userSchema = mongoose.Schema({
-    _id: {type: mongoose.Schema.Types.ObjectId, ref: 'users'},
-    name: {type: String, required: true},
     username: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true},
     birthday: Date,
     favMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'movies' }]
   });
-
-let directorSchema = mongoose.Schema({
-    _id: {type: mongoose.Schema.Types.ObjectId, ref: 'directors'},
-    name: {type: String, required: true},
-    bio: {type: String, required: true},
-    birth: {type: String, required: true}
-})
-
-let genreSchema = mongoose.Schema({
-  _id: {type: mongoose.Schema.Types.ObjectId, ref: 'genres'},
-  name: {type: Staring, required: true},
-  description: {type: String, required: true}
-})
   
   let Movie = mongoose.model('movies', movieSchema);
   let User = mongoose.model('users', userSchema);
-  let Genre = mongoose.model('genres', genreSchema);
-  let Director = mongoose.model('directors', directorSchema);
   
   module.exports.Movie = Movie;
   module.exports.User = User;
-  module.exports.Genre = Genre;
-  module.exports.Director = Director;
